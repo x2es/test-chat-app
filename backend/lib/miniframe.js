8ab422e264fd45ec107e19e05afdd44b6de9d77f
x@ES <deal@coolcab.org>
2015-07-08 10:13:32 +0300

    Implemented miniframe

--- backend/lib/miniframe.js
+++ backend/lib/miniframe.js
@@ -1,22 +1,60 @@
 
 var MiniFrame = {};
 
+function createSetter(propName) {
+  return (function(val) { this['_' + propName] = val; });
+}
+
+function createGetter(propName) {
+  return (function() { return (this['_' + propName]); });
+}
+
 /**
  * Adds accessors (get,set) to specified Class for specified properties
+ * 
+ * Example:
+ *   accessors(Peer, ['uid']);
  *
  * @param {Function} Class
  * @param {Array<String>} properties
  */
-MiniFrame.accessors = function(Class, properties) {};
+MiniFrame.accessors = function(Class, properties) {
+  for (var i=0; i<properties.length; i++) {
+    var prop = properties[i];
+    var suffix = prop.charAt(0).toUpperCase() + prop.slice(1);
+    Class.prototype['get' + suffix] = createGetter(prop);
+    Class.prototype['set' + suffix] = createSetter(prop);
+  }
+};
 
 /**
  * Delegates specified methods of Class to corresponding methods of object,
  * stored in property specified by opts.to
  *
+ * Example:
+ *   delegate(Peer, {
+ *     methods: ['onDisconnected', 'onMessage', 'send']
+ *     to: '_transportFacade', 
+ *   });
+ *
  * @param {Function} Class
- * @param {String} opts.to: instance property of Class
  * @param {Array<String>} opts.methods: list of methods
+ * @param {String} opts.to: instance property of Class
  */
-MiniFrame.delegate = function(Class, opts) {};
+MiniFrame.delegate = function(Class, opts) {
+  var methods = opts.methods;
+  var to = opts.to;
+  for (var i=0; i<methods.length; i++) {
+    var method = methods[i];
+    
+    (function(Class, method, to) {
+      Class.prototype[method] = function() {
+        var target = this[to];
+        var args = Array.prototype.slice.call(arguments);
+        target[method].apply(target, args);
+      };
+    })(Class, method, to);
+  }
+};
 
 module.exports = MiniFrame;
--- backend/spec/miniframe.js
+++ backend/spec/miniframe.js
@@ -1,14 +1,74 @@
 var expect = require('chai').expect;
+var sinon = require('sinon');
+
+var MiniFrame = require('../lib/miniframe.js');
+
+var accessors = MiniFrame.accessors;
+var delegate  = MiniFrame.delegate;
 
 describe('MiniFrame', function() {
   describe('accessors', function() {
-    it('should add get<Property> method');
-    it('should add set<Property> method');
-    it('should persist property');
+    var FooClass;
+
+    beforeEach(function() {
+      FooClass = function() {};
+      accessors(FooClass, ['propBar', 'propBaz']);
+    });
+
+    it('should add get<Property> method to prototype', function() {
+      expect(FooClass.prototype).have.property('getPropBar');
+      expect(FooClass.prototype).have.property('getPropBaz');
+    });
+
+    it('should add set<Property> method to prototype', function() {
+      expect(FooClass.prototype).have.property('setPropBar');
+      expect(FooClass.prototype).have.property('setPropBaz');
+    });
+
+    it('should persist property', function() {
+      var foo = new FooClass();
+
+      foo.setPropBar('barVal');
+      foo.setPropBaz(3);
+
+      expect(foo.getPropBar()).equal('barVal');
+      expect(foo.getPropBaz()).equal(3);
+    });
   });
 
   describe('delegate', function() {
-    it('should delegate');
+    it('should delegate', function() {
+      function Foo() {};
+
+      Foo.prototype.play = function() {};
+      Foo.prototype.stop = function() {};
+
+      function Bar(foo) { this._foo = foo };
+
+      delegate(Bar, {
+        methods: ['play', 'stop'],
+        to: '_foo'
+      });
+
+      var foo = new Foo();
+      sinon.spy(foo, 'play');
+      sinon.spy(foo, 'stop');
+
+      var bar = new Bar(foo);
+
+      expect(bar).have.property('play');
+      expect(bar).have.property('stop');
+
+      bar.play('a/b/c', 123);
+      bar.stop(1);
+
+      expect(foo.play.calledOnce).ok;
+      expect(foo.play.firstCall.args).deep.equal([ 'a/b/c', 123 ]);
+
+      expect(foo.stop.calledOnce).ok;
+      expect(foo.stop.firstCall.args).deep.equal([ 1 ]);
+
+    });
   });
 });
 
