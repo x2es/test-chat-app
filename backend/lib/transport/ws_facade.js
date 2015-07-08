13f628496030a01b96fe988a293b24cc093d5897
x@ES <deal@coolcab.org>
2015-07-08 09:22:53 +0300

    WSFacade implementation

--- backend/lib/transport/ws_facade.js
+++ backend/lib/transport/ws_facade.js
@@ -2,23 +2,33 @@
 /**
  * @extends AbstractTransportFacade
  */
-function WSFacade(wsTransport) {}
+function WSFacade(wsTransport) {
+  this._wsTransport = wsTransport;
+}
 
 /**
  * Disconnection handler
  *
  * @param {DisconnectHandler} handler
  */
-WSFacade.prototype.onDisconnected = function(handler) {};
+WSFacade.prototype.onDisconnected = function(handler) {
+  this._wsTransport.on('close', handler);
+};
 
 /**
  * @param {MessageHandler} handler
  */
-WSFacade.prototype.onMessage = function(handler) {};
+WSFacade.prototype.onMessage = function(handler) {
+  this._wsTransport.on('message', function(messageSerial) {
+    handler.apply(this._wsTransport, [ JSON.parse(messageSerial) ]);
+  });
+};
 
 /**
  * @param {Message} message
  */
-WSFacade.prototype.send = function(message) {};
+WSFacade.prototype.send = function(message) {
+  this._wsTransport.send(JSON.stringify(message));
+};
 
 module.exports = WSFacade;
--- backend/spec/transport/ws_facade.js
+++ backend/spec/transport/ws_facade.js
@@ -1,18 +1,89 @@
 
 var expect = require('chai').expect;
+var sinon = require('sinon');
+
+var WSFacade = require('../../lib/transport/ws_facade.js');
 
 describe('WSFacade', function() {
+  var ws, wsFacade;
+
+  beforeEach(function() {
+    ws = {};
+    wsFacade = null;
+  });
+
   describe('ws disconnected', function() {
-    it('should trigger DisconnectHandler');
+    var disconnectHandler;
+
+    beforeEach(function() {
+      disconnectHandler = null;
+      ws.on = function(eventName, handler) {
+        if (eventName === 'close') disconnectHandler = handler;
+      };
+
+      wsFacade = new WSFacade(ws);
+    });
+
+    it('should trigger DisconnectHandler', function() {
+      var spy = sinon.spy();
+      wsFacade.onDisconnected(spy);
+
+      expect(disconnectHandler).to.be.an('function');
+      disconnectHandler.apply(ws);
+
+      expect(spy.calledOnce).ok;
+    });
   });
 
   describe('ws accept message', function() {
-    it('should trigger MessageHandler');
-    it('should pass message as Object');
+    var messageHandler;
+
+    beforeEach(function() {
+      messageHandler = null;
+      ws.on = function(eventName, handler) {
+        if (eventName === 'message') messageHandler = handler;
+      };
+
+      wsFacade = new WSFacade(ws);
+    });
+
+    it('should trigger MessageHandler', function() {
+      var spy = sinon.spy();
+      wsFacade.onMessage(spy);
+
+      expect(messageHandler).to.be.an('function');
+      messageHandler.apply(ws, ['{}']);
+
+      expect(spy.calledOnce).ok;
+    });
+
+    it('should pass message as Object', function() {
+      var spy = sinon.spy();
+      wsFacade.onMessage(spy);
+
+      var msg = { pay: 'load' };
+      messageHandler.apply(ws, [ JSON.stringify(msg) ]);
+
+      expect(spy.calledOnce).ok;
+      expect(spy.firstCall.args[0]).deep.equal(msg);
+    });
   });
 
   describe('sending message to ws', function() {
-    it('should call ws.send');
-    it('should pass message as serialized json');
+    beforeEach(function() {
+      ws.send = sinon.spy();
+      wsFacade = new WSFacade(ws);
+    });
+
+    it('should call ws.send', function() {
+      wsFacade.send({});
+      expect(ws.send.calledOnce).ok;
+    });
+
+    it('should pass message as serialized json', function() {
+      var msg = { pay: 'load' };
+      wsFacade.send(msg);
+      expect(ws.send.firstCall.args[0]).equal(JSON.stringify(msg));
+    });
   });
 });
